a152fe778ce398671c0ddc9f11be4eef5fe92788
Rishabh Kalra <Rishabh.Kalra@in.niit.com>
2019-11-19 15:00:29 +0530

    mkdir now makes if directory is not present

--- src/modules/read-db/services/database-common-service/databse-common/databse-common.service.ts
+++ src/modules/read-db/services/database-common-service/databse-common/databse-common.service.ts
@@ -7,7 +7,7 @@ import { DatabaseUtilityService } from '../../database-utility-service/database-
 import { SpeakerMergerCoreService } from '../../../../speaker-merger/services/speaker-merger-core-service/speaker-merger-core.service';
 
 export interface WRITE_DIARIZED_FILES_INTERFACE {
-    data: Array<{diarized_data: any, bucket: string, name: string, uri: string}>;
+    data: Array<{ diarized_data: any, bucket: string, name: string, uri: string }>;
 }
 
 @Injectable()
@@ -16,7 +16,7 @@ export class DatabseCommonService {
     constructor(
         private dbUtiiltySrvc: DatabaseUtilityService,
         @Inject(forwardRef(() => SpeakerMergerCoreService))
-        private speakerMergerSrvc: SpeakerMergerCoreService) {}
+        private speakerMergerSrvc: SpeakerMergerCoreService) { }
 
     public DB_URL = path.resolve(__dirname, './../../../../../assets/vis_db');
     public DIARIZATION_DB_URL = path.resolve(__dirname, './../../../../../assets/diarization_db');
@@ -27,7 +27,7 @@ export class DatabseCommonService {
     readJSONdb() {
         console.log('reading from file', this.DB_URL);
         const fileUrl = path.join(this.DB_URL, 'vis_db.json');
-        const fileData = fs.readFileSync(fileUrl, {encoding: 'utf-8'});
+        const fileData = fs.readFileSync(fileUrl, { encoding: 'utf-8' });
         return fileData;
     }
 
@@ -53,7 +53,7 @@ export class DatabseCommonService {
         const url = path.join(this.DIARIZATION_DB_URL, directoryName, fileName);
         console.log('reading ' + url);
         try {
-            const fileContents =  fs.readFileSync(url, {encoding: 'utf-8'});
+            const fileContents = fs.readFileSync(url, { encoding: 'utf-8' });
             return fileContents;
         } catch (e) {
             console.log('error occured while reading file ', fileName);
@@ -67,7 +67,7 @@ export class DatabseCommonService {
      * @returns true if to combined file executed successfully, else false
      */
     appendToCombinedFile(currentFileName, currentFileContents, parentFolderName): boolean {
-        console.log('current file name is ',currentFileName);
+        console.log('current file name is ', currentFileName);
         try {
             const JSONData = JSON.parse(currentFileContents);
             // extract the correct section
@@ -127,10 +127,17 @@ export class DatabseCommonService {
                 const writeUrl = path.join(this.DIARIZATION_DB_URL, folderStructure['parentFolderName']);
                 console.log('write url for file ', folderStructure['fileName'] + ' is ' + writeUrl + '\n\n');
                 try {
-                    fs.mkdirSync(writeUrl, {recursive: true});
+
+                    // check if the directory is already present, else create
+                    if (!fs.existsSync(writeUrl)) {
+                        fs.mkdirSync(writeUrl, { recursive: true });
+                    } else {
+                        console.log(writeUrl + ' already exists, proceeding');
+                    }
+
                     const completeFileUrl = path.join(writeUrl, folderStructure['fileName']);
                     console.log('file path now is ', completeFileUrl);
-                    fs.writeFileSync(completeFileUrl, stringFileData, {encoding: 'utf-8'});
+                    fs.writeFileSync(completeFileUrl, stringFileData, { encoding: 'utf-8' });
                     console.log('file written');
                     totalFilesWritten += 1;
                 } catch (e) {
