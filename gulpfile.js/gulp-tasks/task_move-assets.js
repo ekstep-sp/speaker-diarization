03e52e94ad4c7dde416a27f775685b4386c0f860
Rishabh Kalra <Rishabh.Kalra@in.niit.com>
2019-11-25 10:49:32 +0530

    changed display text for gulpfile tasks

--- gulpfile.js/gulp-tasks/task_move-assets.js
+++ gulpfile.js/gulp-tasks/task_move-assets.js
@@ -1,12 +1,12 @@
 const {src, dest, parallel} = require('gulp');
 
 function moveAssetsTask() {
-    console.log('this is move assets');
+    console.log('moving assets');
     return src(['./src/assets/**/*']).pipe(dest(['./dist/assets']))
 }
 
 function moveConfigTask() {
-    console.log('this is move config');
+    console.log('moving configurations for the project');
     return src(['./src/config/**/*']).pipe(dest(['./dist/config']))
 }
 
